2ba86821116c72bb5d5c5ba578f338c2341b1e10
Jakub Trzebiatowski <cubuspl42@gmail.com>
2017-10-12 21:03:17 +0200

    Implement simple program template model

--- src/main/java/pl/edu/pg/examgeneratorng/ProgramTemplate.java
+++ src/main/java/pl/edu/pg/examgeneratorng/ProgramTemplate.java
@@ -1,8 +1,31 @@
 package pl.edu.pg.examgeneratorng;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Value;
+
+import java.util.List;
+import java.util.Set;
 
 @Value
 class ProgramTemplate {
-    private String content;
+    static abstract class Marker {
+    }
+
+    static class PrologMarker extends Marker {
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class GroupMarker extends Marker {
+        private Group group;
+    }
+
+    @Value
+    static class Line {
+        private String content;
+        private Set<Marker> markers;
+    }
+
+    private List<Line> lines;
 }
--- src/main/java/pl/edu/pg/examgeneratorng/ProgramTemplateLoading.java
+++ src/main/java/pl/edu/pg/examgeneratorng/ProgramTemplateLoading.java
@@ -1,9 +1,13 @@
 package pl.edu.pg.examgeneratorng;
 
+import com.google.common.collect.ImmutableList;
+
 import java.nio.file.Path;
+import java.util.Collections;
 
 final class ProgramTemplateLoading {
     static ProgramTemplate loadProgramTemplate(Path codeTemplatePath) {
-        return new ProgramTemplate("foo bar"); // TODO
+        ProgramTemplate.Line line = new ProgramTemplate.Line("foo bar", Collections.emptySet());
+        return new ProgramTemplate(ImmutableList.of(line)); // TODO
     }
 }
