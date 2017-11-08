4aef3e464581439e86206671b66128f67f0a2c25
Jakub Trzebiatowski <cubuspl42@gmail.com>
2017-11-08 20:24:43 +0100

    Make odftoolkit more quiet

--- src/main/java/pl/edu/pg/examgeneratorng/Main.java
+++ src/main/java/pl/edu/pg/examgeneratorng/Main.java
@@ -1,11 +1,14 @@
 package pl.edu.pg.examgeneratorng;
 
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static pl.edu.pg.examgeneratorng.ExamGeneration.generateAllExamVariants;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Logger.getLogger("org.odftoolkit.odfdom.pkg.OdfXMLFactory").setLevel(Level.WARNING);
         String workspacePath = args[0];
         generateAllExamVariants(Paths.get(workspacePath));
     }
